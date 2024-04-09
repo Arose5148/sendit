c405c26093092f274cffb8f4a8b4a04c96edd184
Arose5148 <166426324+Arose5148@users.noreply.github.com>
2024-04-09 15:40:08 -0400

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,35 @@
+// script.js
+
+// JavaScript code for lightbox functionality
+function openLightbox(img) {
+    var lightbox = document.getElementById("lightbox");
+    var lightboxImage = document.getElementById("lightboxImage");
+    lightboxImage.src = img.src;
+    lightbox.style.display = "block";
+    document.addEventListener("keydown", handleKeyPress);
+    lightbox.addEventListener("click", closeLightbox);
+}
+
+function closeLightbox() {
+    var lightbox = document.getElementById("lightbox");
+    lightbox.style.display = "none";
+    document.removeEventListener("keydown", handleKeyPress);
+    lightbox.removeEventListener("click", closeLightbox);
+}
+
+function handleKeyPress(event) {
+    if (event.key === "Escape") {
+        closeLightbox();
+    }
+}
+
+function handleLightboxClick(event) {
+    var lightbox = document.getElementById("lightbox");
+    if (event.target === lightbox) {
+        closeLightbox();
+    }
+}
+
+// Attach event listener to the lightbox element
+var lightbox = document.getElementById("lightbox");
+lightbox.addEventListener("click", handleLightboxClick);
