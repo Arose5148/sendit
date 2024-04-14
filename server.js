13f563361548b3373c77795772ee9d9d19c27923
Arose5148 <166426324+Arose5148@users.noreply.github.com>
2024-04-13 22:08:38 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const querystring = require('querystring');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,49 @@ const server = http.createServer((req, res) => {
       if (err) throw err;
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(content);
+    });
+  } else if (req.url === '/submit-form' && req.method === 'POST') {
+    let requestBody = '';
+
+    req.on('data', chunk => {
+      requestBody += chunk.toString();
+    });
+
+    req.on('end', () => {
+      const formData = querystring.parse(requestBody);
+      const { name, email, message } = formData;
+
+      // Process the form data and send email
+      const nodemailer = require('nodemailer');
+
+      const transporter = nodemailer.createTransport({
+        // Configure your email service provider settings here
+        // For example, using Gmail SMTP:
+        service: 'gmail',
+        auth: {
+          user: 'sendit.works.node@gmail.com',
+          pass: '7S*Fbk&3diMp^L'
+        }
+      });
+
+      const mailOptions = {
+        from: email,
+        to: 'carmen@sendit.works',
+        subject: 'New message from SendIt contact form',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+      };
+
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log('Error:', error);
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Oops! Something went wrong. Please try again later.');
+        } else {
+          console.log('Email sent:', info.response);
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end('Thank you for your message. We will get back to you soon.');
+        }
+      });
     });
   } else {
     fs.readFile(path.join(__dirname, 'public', req.url), (err, content) => {
