ac8a4e0606f8cea02613d40677b7efb471b0528d
Arose5148 <166426324+Arose5148@users.noreply.github.com>
2024-04-14 00:13:53 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,55 +1,19 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-const nodemailer = require('nodemailer');
 
 const PORT = process.env.PORT || 3000;
-const PUBLIC_DIR = path.join(__dirname, 'public');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST' && req.url === '/submit-form') {
-    let formData = '';
-    req.on('data', chunk => {
-      formData += chunk.toString();
-    });
-    req.on('end', () => {
-      const params = new URLSearchParams(formData);
-      const name = params.get('name');
-      const email = params.get('email');
-      const message = params.get('message');
-
-      const transporter = nodemailer.createTransport({
-        // Configure your email service provider settings here
-        // For example, using Gmail SMTP:
-        service: 'gmail',
-        auth: {
-          user: 'sendit.works.node@gmail.com',
-          pass: 'wkvk amjh bayc hwmi'
-        }
-      });
-
-      const mailOptions = {
-        from: email,
-        to: 'carmen@sendit.works',
-        subject: 'New message from SendIt contact form',
-        text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`
-      };
-
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log('Error:', error);
-          res.writeHead(500, { 'Content-Type': 'text/plain' });
-          res.end('Oops! Something went wrong. Please try again later.');
-        } else {
-          console.log('Email sent:', info.response);
-          res.writeHead(200, { 'Content-Type': 'text/plain' });
-          res.end('Thank you for your message. We will get back to you soon.');
-        }
-      });
+  // Serve static files from the 'public' directory
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+      if (err) throw err;
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content);
     });
   } else {
-    const filePath = path.join(PUBLIC_DIR, req.url === '/' ? 'index.html' : req.url);
-    fs.readFile(filePath, (err, content) => {
+    fs.readFile(path.join(__dirname, 'public', req.url), (err, content) => {
       if (err) {
         if (err.code === 'ENOENT') {
           res.writeHead(404);
